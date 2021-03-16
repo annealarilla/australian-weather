7fd501d41df34c3901db40590382a27fc02041c3
Anne Alarilla <anne.alarilla@health.org.uk>
2021-03-16 15:13:21 +0000

    Changing the colour into purple

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour="darkred")+ 
+  geom_point(colour="purple")+ 
   facet_wrap(~WindDir9am)+
   labs(title= city, x="Temperature", y="Humidity")+
   theme_minimal()
